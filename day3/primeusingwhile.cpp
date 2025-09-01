ab0f3c8c727884b47858e3b3f87e392c1dca4670
AyaanWiz <121355752+AyaanWiz@users.noreply.github.com>
2025-09-02 00:08:49 +0530

    my day 3 of DSA , i sloved two questions of DSA

---
+++ day3/primeusingwhile.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int i = 2;
+
+    while(i<n){
+
+        if (n % i == 0){
+
+            cout<<"number is not prime"<<endl;
+
+        }
+
+        else{
+
+           cout<< "number is prime"<<endl;
+
+        }
+
+       i = i + 1;
+
+    }
+
+     
+
+}
+
+
