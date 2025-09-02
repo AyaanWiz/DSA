fb50dc395267c34c4497600a6cd57cf676fb00b0
AyaanWiz <121355752+AyaanWiz@users.noreply.github.com>
2025-09-02 23:59:33 +0530

    my day 4 of DSA i solved 2 questions of DSA

--- day3/pattern1.cpp
+++ day3/pattern1.cpp
@@ -4,21 +4,25 @@ using namespace std;
 int main(){
 
 int n;
-cin >> n;
+cin>>n;
 
 int i = 1;
 
 while(i<=n){
 
-    int j = 1;
+  int j = 1;
 
-    while(j<=n){
-      cout <<"*";
-      j = j + 1;
-    }
+  while(j<=n){
+
+    cout<<"*";
+    j = j + 1;
+  }
 
     cout<<endl;
-      i = i + 1;
+
+    i = i + 1;  
+
 }
+
 
 }
